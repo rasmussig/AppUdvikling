ea5cd4cd28021bcc1ab60a45d56e783f527c5d29
rasmussig <rasmus2001@live.dk>
2023-08-31 11:53:56 +0200

    Modul2

---
+++ Modul2/Algoritme.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Modul2
+{
+    internal class Algoritme
+    {
+        public void Run()
+        {
+            int FindMax(int[] a)
+            {
+                int max = a[0];
+                int i = 1;
+                while (i < a.Length)
+                {
+                    if (a[i] > max)
+                    {
+                        max = a[i];
+                    }
+                    i++;
+                }
+                return max;
+            }
+
+          
+
+            int[] array2 = { 10, 20, 30, 40, 50 };
+            int max2 = FindMax(array2);
+            Console.WriteLine("Det største tal i array2 er: " + max2);
+
+        }
+    }
+}
--- Modul2/Program.cs
+++ Modul2/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            new Opgave1().Run();
         }
     }
 }
