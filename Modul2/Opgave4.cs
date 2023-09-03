e4b1f5047b411c4a885930e049bc3660b499bd02
rasmussig <rasmus2001@live.dk>
2023-09-03 19:22:41 +0200

    Opg 4 laptop

--- Modul2/Opgave4.cs
+++ Modul2/Opgave4.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,29 @@ namespace Modul2
 {
     internal class Opgave4
     {
+        public void Run()
+        {
+             bool IsPalindrom(string input)
+            {
+                int start = 0;
+                int end = input.Length - 1;
+
+                while (start < end)
+                {
+                    if (input[start] != input[end])
+                    {
+                        return false;
+                    }
+                    start++;
+                    end--;
+                }
+                return true;
+            }
+
+            Console.WriteLine("Skriv en text, vi tjekker om det er et palindrom: ");
+            string input = Console.ReadLine().ToLower();
+
+            
+        }
     }
 }
