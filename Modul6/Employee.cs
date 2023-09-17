674d42b215f5679d608d42931da862df9af65310
rasmussig <rasmus2001@live.dk>
2023-09-17 20:48:00 +0200

    Yes

---
+++ Modul6/Employee.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+namespace Modul6
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public int TaxDeduction { get; set; }
+        public int TaxPrecentage { get; set; }
+
+        public bool IsMemberOfLunch { get; set; } = false;
+        public bool IsMemberGiftbox { get; set; } = false;
+
+        public virtual int Salary { get; }
+    }
+}
