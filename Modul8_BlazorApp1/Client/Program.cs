92d7955ca217e0b702ccd3657aa2e83908a3e458
rasmussig <rasmus2001@live.dk>
2023-09-27 08:21:51 +0200

    Yes

--- Modul8_BlazorApp1/Client/Program.cs
+++ Modul8_BlazorApp1/Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Modul8_BlazorApp1.Client;
+using Blazored.LocalStorage;
 
 namespace Modul8_BlazorApp1.Client
 {
@@ -11,6 +12,8 @@ namespace Modul8_BlazorApp1.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddBlazoredLocalStorage();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
