9c9d5f841c198cdb2a6c678a4009c435f42450d9
rasmussig <rasmus2001@live.dk>
2023-09-28 11:46:37 +0200

    Modul 10 - Opg 1,2,3

---
+++ Modul8_BlazorApp1/Client/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modul8_BlazorApp1.Client.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Name must be less than 50 characters.")]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool IsPublished { get; set; }
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+        public string Category { get; set; }
+    }
+}
--- Modul8_BlazorApp1/Client/Model/TodoItem.cs
+++ Modul8_BlazorApp1/Client/Model/TodoItem.cs
@@ -1,5 +1,10 @@
-﻿public class ToDoItem
+﻿using System;
+namespace Modul8_BlazorApp1.Client.Model
+{ 
+
+public class ToDoItem
 {
     public string? Title { get; set; } //? indikere at egenskaben kan have en værdi af "Null"
     public bool IsDone { get; set; }
+}
 }
