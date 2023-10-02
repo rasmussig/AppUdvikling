792a836ddfe6765b0975ce91d77c9aeba0101853
rasmussig <rasmus2001@live.dk>
2023-10-02 18:53:04 +0200

    Yeees todo2

--- Modul8_BlazorApp1/Client/Model/TodoItem.cs
+++ Modul8_BlazorApp1/Client/Model/TodoItem.cs
@@ -7,12 +7,12 @@ public class ToDoItem : IComparable<ToDoItem>
     public string? Title { get; set; } //? indikere at egenskaben kan have en værdi af "Null"
     public bool IsDone { get; set; }
 
-    public string Priority { get; set; }
+    public int Priority { get; set; } = 1;
 
     public int CompareTo(ToDoItem other)
         {
             // Sammenlign prioritet ved at sammenligne deres strengværdier
-            return string.Compare(Priority, other.Priority, StringComparison.OrdinalIgnoreCase);
+            return Priority.CompareTo(other.Priority);
         }
     }
 }
