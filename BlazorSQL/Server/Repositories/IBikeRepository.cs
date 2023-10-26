92ec7d4a1b551aa4f3e76f23a33c5bdf4086767a
rasmussig <rasmus2001@live.dk>
2023-10-26 12:45:51 +0200

    Yes

---
+++ BlazorSQL/Server/Repositories/IBikeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BlazorSQL.Client.Pages;
+using BlazorSQL.Shared;
+namespace BlazorSQL.Server.Repositories
+{
+    public interface IBikeRepository
+    {
+        BEBike[] GetAll();
+        void Add(BEBike bike);
+        void DeleteById(int id);
+    }
+}
---
+++ BlazorSQL/Shared/BEBike.cs
@@ -0,0 +1,13 @@
+using System;
+namespace BlazorSQL.Shared
+{
+    public class BEBike
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = String.Empty;
+        public string Model { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public int Price { get; set; } = 0;
+        public string? ImageUrl { get; set; } = String.Empty;
+    }
+}
