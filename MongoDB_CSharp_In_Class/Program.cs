2f779e4c8d054704b5c5e7cb62310dd6d9b6f79c
rasmussig <rasmus2001@live.dk>
2023-11-02 18:38:19 +0100

    Yes

---
+++ MongoDB_CSharp_In_Class/Program.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Bson;
+using MongoDB_CSharp_In_Class.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB_CSharp_In_Class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var doc = new BsonDocument
+            {
+                {"Name","Rasmus" },
+                 {"Email","rasmus2001@live.dk" },
+                  {"Tlf","24246503" }
+            };
+
+            Persistency persistency = new Persistency();
+
+            Console.WriteLine("---------- Opret ny ansatte --------------");
+            persistency.opretteAnsat(doc);
+
+            Console.WriteLine("--------Få alle ansatte------------");           
+            var result = persistency.GetAll();
+
+            foreach (var item in result)
+            {
+
+                Console.WriteLine(item.ToString());
+                //Console.WriteLine("Navn: " + document["Name"]);
+                //Console.WriteLine("Email: " + document["Email"]);
+                //Console.WriteLine("Tlf: " + document["Tlf"]);
+                //Console.WriteLine("----------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MongoDB_CSharp_In_Class/Storage/Persistency.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDB_CSharp_In_Class.Storage
+{
+    public class Persistency
+    {
+        private string connectionString = "mongodb+srv://rasmussig:kagemand123@cluster0.6vg8bbv.mongodb.net/";
+
+        IMongoClient dbClient;
+
+        IMongoDatabase database;
+
+        //= dbClient.GetDatabase("ansattedb");
+
+        IMongoCollection<BsonDocument> collection;
+
+        public Persistency()
+        {
+            dbClient = new MongoClient(connectionString);
+
+            database = dbClient.GetDatabase("ansattedb");
+
+            collection = database.GetCollection<BsonDocument>("projects");
+        }
+
+        public void opretteAnsat(BsonDocument ansatte)
+        {
+            collection.InsertOne(ansatte);
+        }
+
+        public List<BsonDocument> GetAll()
+        {
+            return collection.Find(new BsonDocument()).ToList();
+        }
+
+    }
+}
